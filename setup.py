379488ee2980e1b33753d098d88fb1139a69deeb
Firemark <marpiechula@gmail.com>
2014-10-13 01:04:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="hs-onliner",
+    version="0.0.1",
+    author="Firemark & Kytes",
+    author_email="marpiechula@gmail.com",
+    description="Site to view who will be in hackerspace every week."
+    license="MIT",
+    keywords="example documentation tutorial",
+    url="https://github.com/firemark/hs-onliner",
+    packages=find_packages(),
+    install_requires=(
+        'Flask==0.10.1'
+    )
+)
