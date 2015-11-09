03e8a939f06d2781986eb504f4220a753a51fd09
Firemark <marpiechula@gmail.com>
2015-11-09 03:25:58 +0100

    add editable event

--- assets/js/eventTemplate.js
+++ assets/js/eventTemplate.js
@@ -2,11 +2,12 @@ var EventTemplate = React.createClass({displayName: "EventTemplate",
     getInitialState: function() {
         return {
             participants: [],
-            editable: false
+            isEditable: false
         };
     },
     render: function() {
         var attrs = this.props.event.attributes;
+        var isEditable = this.state.isEditable;
         var is_logged = this.props.is_logged;
         var date = attrs.date.split('-');
 
@@ -17,24 +18,45 @@ var EventTemplate = React.createClass({displayName: "EventTemplate",
                     React.createElement("small", null, date[2])
                 ), 
                 React.createElement("div", {className: "main"}, 
-                    React.createElement("h1", null, 
-                        attrs.topic || 'general event', 
-                        React.createElement(If, {cond: is_logged}, 
-                            React.createElement("button", {className: "edit-event"}, "✎")
-                        )
-                    ), 
-                    React.createElement("div", {className: "info"}, 
-                        React.createElement("time", {className: "start"}, 
-                            React.createElement("strong", null, "Start: "), 
-                            attrs.time_start
+                    React.createElement(If, {cond: !isEditable}, 
+                        React.createElement("h1", null, 
+                            attrs.topic, 
+                            React.createElement(If, {cond: is_logged}, 
+                                React.createElement("button", {className: "edit-event del"}, "❌"), 
+                                React.createElement("button", {className: "edit-event", onClick: this.edit}, "✎")
+                            )
                         ), 
-                        React.createElement("time", {className: "end"}, 
-                            React.createElement("strong", null, "End: "), 
-                            attrs.time_end
-                        )
+                        React.createElement("div", {className: "info"}, 
+                            React.createElement("time", {className: "start"}, 
+                                React.createElement("strong", null, "Start: "), 
+                                attrs.time_start
+                            ), 
+                            React.createElement("time", {className: "end"}, 
+                                React.createElement("strong", null, "End: "), 
+                                attrs.time_end
+                            )
+                        ), 
+                        React.createElement("p", null, attrs.description)
+                    ), 
+                    React.createElement(If, {cond: isEditable && is_logged}, 
+                        React.createElement("h1", null, 
+                            React.createElement("input", {value: attrs.topic}), 
+                            React.createElement("button", {className: "edit-event del"}, "❌"), 
+                            React.createElement("button", {className: "edit-event"}, "✔")
+                        ), 
+                        React.createElement("div", {className: "info"}, 
+                            React.createElement("time", {className: "start"}, 
+                                React.createElement("strong", null, "Start: "), 
+                                React.createElement("input", {value: attrs.time_start})
+                            ), 
+                            React.createElement("time", {className: "end"}, 
+                                React.createElement("strong", null, "End: "), 
+                                React.createElement("input", {value: attrs.time_end})
+                            )
+                        ), 
+                        React.createElement("textarea", null, attrs.description)
                     ), 
 
-                    React.createElement("p", null, attrs.description || '-'), 
                     React.createElement(ParticipantsTemplate, {
                       participants: this.state.participants, 
                       is_logged: is_logged})
@@ -43,8 +65,12 @@ var EventTemplate = React.createClass({displayName: "EventTemplate",
         );
     },
     componentWillMount: function () {
+        this.setState({isEditable: this.props.event.isNew()});
         this.props.event.participants.on("update", function (participants) {
             this.setState({participants: participants});
         }.bind(this));
+    },
+    edit: function () {
+        this.setState({isEditable: true});
     }
 });
--- assets/jsx/eventTemplate.js
+++ assets/jsx/eventTemplate.js
@@ -2,11 +2,12 @@ var EventTemplate = React.createClass({
     getInitialState: function() {
         return {
             participants: [],
-            editable: false
+            isEditable: false
         };
     },
     render: function() {
         var attrs = this.props.event.attributes;
+        var isEditable = this.state.isEditable;
         var is_logged = this.props.is_logged;
         var date = attrs.date.split('-');
 
@@ -17,24 +18,45 @@ var EventTemplate = React.createClass({
                     <small>{date[2]}</small>
                 </div>
                 <div className='main'>
-                    <h1>
-                        {attrs.topic || 'general event'}
-                        <If cond={is_logged}>
-                            <button className="edit-event">✎</button>
-                        </If>
-                    </h1>
-                    <div className='info'>
-                        <time className='start'>
-                            <strong>Start: </strong>
-                            {attrs.time_start}
-                        </time>
-                        <time className='end'>
-                            <strong>End: </strong>
-                            {attrs.time_end}
-                        </time>
-                    </div>
+                    <If cond={!isEditable}>
+                        <h1>
+                            {attrs.topic}
+                            <If cond={is_logged}>
+                                <button className="edit-event del">❌</button>
+                                <button className="edit-event" onClick={this.edit}>✎</button>
+                            </If>
+                        </h1>
+                        <div className='info'>
+                            <time className='start'>
+                                <strong>Start: </strong>
+                                {attrs.time_start}
+                            </time>
+                            <time className='end'>
+                                <strong>End: </strong>
+                                {attrs.time_end}
+                            </time>
+                        </div>
+                        <p>{attrs.description}</p>
+                    </If>
+                    <If cond={isEditable && is_logged}>
+                        <h1>
+                            <input value={attrs.topic} />
+                            <button className="edit-event del">❌</button>
+                            <button className="edit-event">✔</button>
+                        </h1>
+                        <div className='info'>
+                            <time className='start'>
+                                <strong>Start: </strong>
+                                <input value={attrs.time_start} />
+                            </time>
+                            <time className='end'>
+                                <strong>End: </strong>
+                                <input value={attrs.time_end} />
+                            </time>
+                        </div>
+                        <textarea>{attrs.description}</textarea>
+                    </If>
 
-                    <p>{attrs.description || '-'}</p>
                     <ParticipantsTemplate
                       participants={this.state.participants}
                       is_logged={is_logged} />
@@ -43,8 +65,12 @@ var EventTemplate = React.createClass({
         );
     },
     componentWillMount: function () {
+        this.setState({isEditable: this.props.event.isNew()});
         this.props.event.participants.on("update", function (participants) {
             this.setState({participants: participants});
         }.bind(this));
+    },
+    edit: function () {
+        this.setState({isEditable: true});
     }
 });
